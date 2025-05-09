2b09dd3c22f1219cc578288286d7d20623dbde02
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-08 17:32:37 -0700

    Added session and flash

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
+const session = require('express-session');
+const flash = require('connect-flash');
+
 const methodOverride = require('method-override');
+const ExpressError = require('./utils/ExpressError');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
     .then(() => {
         console.log('MongoDB Connection Established');
     })
@@ -28,6 +36,26 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+};
+app.use(session(sessionConfig));
+app.use(flash());
+
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 
 app.get('/', (req, res) => {
@@ -35,13 +63,15 @@ app.get('/', (req, res) => {
     res.redirect('/campgrounds')
 });
 
-
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
 
+// app.use('*', (req, res, next) => {
+//     next(new ExpressError('No Page Found', 404));
+// });
+// must come after all other routes
 
-// app.all('*', (req, res, next) => {
-//     console.warn(`Unmatched route: ${req.method} ${req.originalUrl}`);
+// app.use((req, res, next) => {
 //     next(new ExpressError('No Page Found', 404));
 // });
 
