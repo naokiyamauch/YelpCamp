b1eeb9517ddbd98a00ca76de5888f1aca3b2ef78
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-09 16:29:00 -0700

    feat: added login feature

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
@@ -20,5 +21,21 @@ router.post('/register', async (req, res) => {
 		res.redirect('register');
 	}
 });
+
+router.get('/login', (req, res) => {
+	res.render('users/login');
+});
+
+router.post(
+	'/login',
+	passport.authenticate('local', {
+		failureFlash: true,
+		failureRedirect: '/login',
+	}),
+	async (req, res) => {
+		req.flash('success', 'Welcome Back!');
+		res.redirect('/campgrounds');
+	}
+);
 
 module.exports = router;
