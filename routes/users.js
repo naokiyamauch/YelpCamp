5e9366f681827804a75a618c83580e927c8992d7
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-09 17:11:35 -0700

    feat: added 'return to original url' feature when asking login | style: added login/register/logout btn to navbar

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,20 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const User = require('../models/user');
-const catchAsync = require('../utils/catchAsync');
+const { storeReturnTo } = require('../middleware');
 
 router.get('/register', (req, res) => {
 	res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
 	try {
 		const { email, username, password } = req.body;
 		const user = new User({ email, username });
 		const registeredUser = await User.register(user, password);
-		console.log(registeredUser);
+		req.login(registeredUser, (err) => {
+			if (err) return next(err);
+		});
 		req.flash('success', 'Welcome to Yelp Camp!');
 		res.redirect('/campgrounds');
 	} catch (error) {
@@ -28,13 +30,15 @@ router.get('/login', (req, res) => {
 
 router.post(
 	'/login',
+	storeReturnTo,
 	passport.authenticate('local', {
 		failureFlash: true,
 		failureRedirect: '/login',
 	}),
 	(req, res) => {
 		req.flash('success', 'Welcome Back!');
-		res.redirect('/campgrounds');
+		const redirectUrl = res.locals.returnTo || '/campgrounds';
+		res.redirect(redirectUrl);
 	}
 );
 
