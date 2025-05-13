cd2e852771e778444380f43a6b37c5e8f60e0224
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-13 11:27:58 -0700

    feat: add image upload feature to edit

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -51,6 +51,12 @@ module.exports.updateCampground = async (req, res) => {
 	const campground = await Campground.findByIdAndUpdate(id, {
 		...req.body.campground,
 	});
+	const images = req.files.map((f) => ({
+		url: f.path,
+		filename: f.filename,
+	}));
+	campground.images.push(...images);
+	await campground.save();
 	req.flash('success', 'Successfully Updated');
 	res.redirect(`/campgrounds/${campground._id}`);
 };
